983b20e27b5d7981cf4de49a3991c5ddf97e9e33
Kelvin Liebertz <kelvinliebertz@googlemail.com>
2013-12-02 18:21:00 +0100

    Created a rails applichation and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Streetscout::Application.config.secret_key_base = '56be712c4489a6272ee1517559cfdab8e1c13c3446bbf69f2672d51445bd131512df062ecd9e655de3831919e411a7a380b9fc96d9d9c1c75dacea078be9501e'
